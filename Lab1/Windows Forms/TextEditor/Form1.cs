2d04c3c80f412e4943be8fff3a48972def7fdb9f
Maria Marchenko <mariamarcenko27@gmail.com>
2026-02-17 18:51:41 +0200

    first lab

---
+++ Lab1/Windows Forms/TextEditor/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace TextEditor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Очищення файлу/відкриття нового
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textField.Clear();
+        }
+
+        // Відкриття файлу
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textField.Text = File.ReadAllText(openFileDialog1.FileName);
+            }
+        }
+
+        // Збереження файлу
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(saveFileDialog1.FileName, textField.Text);
+            }
+        }
+
+        // Закриття програми
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
